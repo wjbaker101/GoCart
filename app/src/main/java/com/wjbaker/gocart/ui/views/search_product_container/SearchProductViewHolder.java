e915ad2a01fd80759a7a0df3f3177dd76201b65d
wjbaker101 <willbaker10198@gmail.com>
2018-04-05 22:55:44 +0100

    Move bind functionality

--- app/src/main/java/com/wjbaker/gocart/ui/views/search_product_container/SearchProductViewHolder.java
+++ app/src/main/java/com/wjbaker/gocart/ui/views/search_product_container/SearchProductViewHolder.java
@@ -2,6 +2,16 @@ package com.wjbaker.gocart.ui.views.search_product_container;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.wjbaker.gocart.R;
+import com.wjbaker.gocart.shopping.Product;
+import com.wjbaker.gocart.shopping.ShoppingList;
+import com.wjbaker.gocart.ui.views.product_item.ProductItemSearchView;
 
 /**
  * Created by William on 15/03/2018.
@@ -15,5 +25,57 @@ public class SearchProductViewHolder extends RecyclerView.ViewHolder
         super(searchProductView);
 
         this.searchProductView = searchProductView;
+    }
+
+    /**
+     * Updates the content of the View.
+     *
+     * @param product The Product associated with the View.
+     */
+    public void bind(Product product)
+    {
+        TextView productName = this.searchProductView.findViewById(R.id.product_item_search_name);
+        TextView productCost = this.searchProductView.findViewById(R.id.product_item_search_cost);
+        final ImageView image = this.searchProductView.findViewById(R.id.product_item_search_image);
+
+        ProductItemSearchView productItemView = this.searchProductView.findViewById(R.id.product_item_search_content);
+
+        productName.setText(product.getName());
+        productCost.setText(String.format("£%.2f", product.getCost()));
+        Picasso.get().load(product.getImageURL()).into(image);
+
+        productItemView.setProduct(product);
+
+        if (ShoppingList.getInstance(this.searchProductView.getContext()).get(product.getTPNB()) != null)
+        {
+            CheckBox checkBox = this.searchProductView.findViewById(R.id.product_item_search_checked);
+
+            checkBox.setChecked(true);
+
+            checkBox.setOnCheckedChangeListener(this.getOnCheckBoxChangeListener(product));
+        }
+    }
+
+    /**
+     * Adds the event listener for when the CheckBox checked state changes.<br>
+     * Will add or remove the associated Product from the shopping list.
+     */
+    public CompoundButton.OnCheckedChangeListener getOnCheckBoxChangeListener(final Product product)
+    {
+        return new CompoundButton.OnCheckedChangeListener()
+        {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean checked)
+            {
+                if (checked)
+                {
+                    ShoppingList.getInstance(searchProductView.getContext()).addItem(product);
+                }
+                else
+                {
+                    ShoppingList.getInstance(searchProductView.getContext()).removeItem(product.getTPNB());
+                }
+            }
+        };
     }
 }
