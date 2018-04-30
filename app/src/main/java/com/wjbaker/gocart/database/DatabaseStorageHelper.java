6ea02b0a1675c6c1cdcb939f6728a4910a423103
wjbaker101 <willbaker10198@gmail.com>
2018-04-30 13:31:01 +0100

    Create initial tables

--- app/src/main/java/com/wjbaker/gocart/database/DatabaseStorageHelper.java
+++ app/src/main/java/com/wjbaker/gocart/database/DatabaseStorageHelper.java
@@ -6,6 +6,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.text.TextUtils;
 
 import com.wjbaker.gocart.database.tables.ShoppingListTable;
+import com.wjbaker.gocart.database.tables.TescoStoresTable;
 import com.wjbaker.gocart.shopping.ShoppingList;
 
 /**
@@ -18,7 +19,7 @@ public class DatabaseStorageHelper extends SQLiteOpenHelper
     /**
      * Stores the current version of the database.
      */
-    public static final int DATABASE_VERSION = 3;
+    public static final int DATABASE_VERSION = 4;
 
     /**
      * Stores the name of the database.
@@ -32,6 +33,31 @@ public class DatabaseStorageHelper extends SQLiteOpenHelper
 
     @Override
     public void onCreate(SQLiteDatabase database)
+    {
+        this.createProductsTable(database);
+        this.createStoreTable(database);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion)
+    {
+        database.execSQL(String.format("DROP TABLE IF EXISTS %s", ShoppingListTable.TABLE_NAME));
+        database.execSQL(String.format("DROP TABLE IF EXISTS %s", TescoStoresTable.TABLE_NAME));
+        this.onCreate(database);
+    }
+
+    @Override
+    public void onDowngrade(SQLiteDatabase database, int oldVersion, int newVersion)
+    {
+        this.onUpgrade(database, oldVersion, newVersion);
+    }
+
+    /**
+     * Executes an SQL query to create the products table.
+     *
+     * @param database The database to query.
+     */
+    private void createProductsTable(SQLiteDatabase database)
     {
         String[] columnInformation =
         {
@@ -52,16 +78,51 @@ public class DatabaseStorageHelper extends SQLiteOpenHelper
         database.execSQL(String.format("CREATE TABLE %s (%s)", ShoppingListTable.TABLE_NAME, columns));
     }
 
-    @Override
-    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion)
+    /**
+     * Executes an SQL query to create the user's store table.
+     *
+     * @param database The database to query.
+     */
+    private void createStoreTable(SQLiteDatabase database)
     {
-        database.execSQL(String.format("DROP TABLE IF EXISTS %s", ShoppingListTable.TABLE_NAME));
-        this.onCreate(database);
-    }
+        String[] columnInformation =
+        {
+            String.format("%s TEXT", TescoStoresTable.KEY_NAME),
 
-    @Override
-    public void onDowngrade(SQLiteDatabase database, int oldVersion, int newVersion)
-    {
-        this.onUpgrade(database, oldVersion, newVersion);
+            String.format("%s REAL", TescoStoresTable.KEY_LOCATION_LONGITUDE),
+            String.format("%s REAL", TescoStoresTable.KEY_LOCATION_LATITUDE),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_MONDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_MONDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_MONDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_TUESDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_TUESDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_TUESDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_WEDNESDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_WEDNESDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_WEDNESDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_THURSDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_THURSDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_THURSDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_FRIDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_FRIDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_FRIDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_SATURDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_SATURDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_SATURDAY_CLOSING),
+
+            String.format("%s BOOLEAN", TescoStoresTable.KEY_SUNDAY_IS_OPEN),
+            String.format("%s INTEGER", TescoStoresTable.KEY_SUNDAY_OPENING),
+            String.format("%s INTEGER", TescoStoresTable.KEY_SUNDAY_CLOSING),
+        };
+
+        String columns = TextUtils.join(", ", columnInformation);
+
+        database.execSQL(String.format("CREATE TABLE %s (%s)", TescoStoresTable.TABLE_NAME, columns));
     }
 }
